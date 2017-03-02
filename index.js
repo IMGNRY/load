ab3e8c8e65a45bd1f0c2b14057c6aa46e2850fc3
Fille Åström <fille@imgnry.com>
2017-03-02 12:34:28 +0100

    switched to a safer usage of the module
    i was also having problems while working with it in npm linked mode

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
-(function (env) {
-	var Gun;
-	if(typeof module !== "undefined" && module.exports){ Gun = require('gun/gun') }
-	if(typeof window !== "undefined"){ Gun = window.Gun }
 
-    Gun.chain.load = function(cb) {
-        const gun = this, root = gun.back(-1)
-        return this.val((obj, key) => {
+module.exports = function(Gun, globalOpt) {
+	globalOpt = Object.assign({
+		skipnull: true,
+		log: false
+	}, globalOpt)
+   	Gun.chain.load = function(cb, opt) {
+	   opt = Object.assign(globalOpt, opt)
+       const gun = this, root = gun.back(-1)
+       return this.val((obj, key) => {
 
-            obj = Gun.obj.copy(obj);
-            //const start = Date.now()
-            const queue = {}
-            let doc, done
+		   // if null or undefined (but shouldnt be able to be that, right ?).. skip it if opt allows
+		   if (obj == null && opt.skipnull) {
+				if (opt.log) {
+					console.log('skipping null');
+				}
+				return
+		   }
 
-            function expand(o) {
-                if (!doc) {
-                    doc = o
-                }
-                Gun.obj.map(o, (val, prop) => {
-                    const soul = Gun.val.rel.is(val)
-                    if (soul) {
-                        queue[soul] = true
-                        root.get(soul).val(loadedValue => {
-                            queue[soul] = false
-                            loadedValue = Gun.obj.copy(loadedValue)
-                            o[prop] = loadedValue
-                            expand(loadedValue)
-                        })
-                        return
-                    }
+           obj = Gun.obj.copy(obj);
+           const queue = {}
+           let doc, done
 
-                    // if it doesnt have a soul, just attach the value as is
-                    o[prop] = val
-                })
-                const wait = Gun.obj.map(queue, wait => {
-                    if (wait) {
-                        return true
-                    }
-        		});
-                if (done || wait) {
-                    // dont send the document back yet, we are still waiting for it load completely
-                    return
-                }
-                done = true;
-                cb(doc, key)
-            }
+           function expand(o) {
+               if (!doc) {
+                   doc = o
+               }
+               Gun.obj.map(o, (val, prop) => {
+                   const soul = Gun.val.rel.is(val)
+                   if (soul) {
+                       queue[soul] = true
+                       root.get(soul).val(loadedValue => {
+                           queue[soul] = false
+                           loadedValue = Gun.obj.copy(loadedValue)
+                           o[prop] = loadedValue
+                           expand(loadedValue)
+                       })
+                       return
+                   }
 
-            expand(obj)
-        })
-    }
-}());
+                   // if it doesnt have a soul, just attach the value as is
+                   o[prop] = val
+               })
+               const wait = Gun.obj.map(queue, wait => {
+                   if (wait) {
+                       return true
+                   }
+       			});
+               if (done || wait) {
+                   // dont send the document back yet / or again, we are either already done and have sent the doc, or are still waiting for it load completely
+                   return
+               }
+               done = true;
+               cb(doc, key)
+           }
+
+           expand(obj)
+       })
+   }
+}
